1c2b34aa5c49d914b318dcb421a316c99c16edb4
srzvan <sbingu.razvan@gmail.com>
2021-05-26 21:21:14 +0300

    extracted the daily stock time series service & moved all enums/types/interfaces from stock-api-types.ts

--- src/components/StockTimeSeries.tsx
+++ src/components/StockTimeSeries.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { Stack, chakra, Tag, TagLabel, HStack } from "@chakra-ui/react";
+import { extent } from "d3-array";
 import { Range } from "react-date-range";
+import { Stack, chakra, Tag, TagLabel, HStack } from "@chakra-ui/react";
 
 import StockChart from "./StockChart";
 import FilterStockTimeSeries from "./FilterStockTimeSeries";
 import StockTimeSeriesLoader from "./StockTimeSeriesLoader";
-
+import { getDailyStockTimeSeries } from "../services/getDailyStockTimeSeries";
 import { filterTimeSeries, NormalizedTimeSeries, normalizeStockData } from "../utils/daily-stock-time-series";
-import { extent } from "d3-array";
 
 export type DateInterval = {
   key: string;
@@ -35,31 +35,22 @@ function StockTimeSeries(props: StockTimeSeriesProps) {
       fetchDailyStockTimeSeries();
     }
 
-    /* ******************************** */
     async function fetchDailyStockTimeSeries() {
-      try {
-        setIsLoading(true);
-        const apiKey = process.env.REACT_APP_ALPHA_VANTAGE_API_KEY;
-        let response = await fetch(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${query}&outputsize=full&apikey=${apiKey}`
-        );
-        let stockTimeSeries = await response.json();
+      setIsLoading(true);
+      let stockTimeSeries = await getDailyStockTimeSeries(query);
 
-        if (stockTimeSeries) {
-          let normalizedData = normalizeStockData(stockTimeSeries);
-          let [min, max] = extent(normalizedData, dataPoint => dataPoint.date);
+      if (stockTimeSeries) {
+        let normalizedData = normalizeStockData(stockTimeSeries);
+        let [min, max] = extent(normalizedData, dataPoint => dataPoint.date);
 
-          setMinDate(min!);
-          setMaxDate(max!);
+        setMinDate(min!);
+        setMaxDate(max!);
 
-          setFullStockTimeSeries(normalizedData);
-          setShouldFetchDailyStockTimeSeries(false);
-          setIsLoading(false);
-        } else {
-          throw new Error("There is no data for the selected company 😕");
-        }
-      } catch (err) {
-        console.error(err);
+        setFullStockTimeSeries(normalizedData);
+        setShouldFetchDailyStockTimeSeries(false);
+        setIsLoading(false);
+      } else {
+        throw new Error("There is no data for the selected company 😕");
       }
     }
   }, [shouldFetchDailyStockTimeSeries]);
@@ -104,12 +95,12 @@ function StockTimeSeries(props: StockTimeSeriesProps) {
     </HStack>
   ) : (
     <chakra.p
-      gridColumn="2 / -2"
-      gridRow="1 / span 3"
       fontSize="3xl"
       display="flex"
-      justifyContent="center"
+      gridColumn="2 / -2"
       alignItems="center"
+      gridRow="1 / span 3"
+      justifyContent="center"
     >
       Search for a company to load its stock data. Afterwards you can filter it using the calendar.
     </chakra.p>
